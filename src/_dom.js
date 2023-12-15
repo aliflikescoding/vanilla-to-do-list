41fbd795dbe47642ea42699010e7a55849634cdb
aliflikescoding <alif.likes.coding@gmail.com>
2023-12-15 09:19:11 +0700

    Add show form function and export it

--- src/_dom.js
+++ src/_dom.js
@@ -1,5 +1,13 @@
 const Dom = (() => {
+  const showForm = (form) => {
+    if (form.classList.contains("show")) {
+      form.classList.remove("show");
+    }
+  };
 
+  return {
+    showForm: showForm,
+  }
 })();
 
 export default Dom;
