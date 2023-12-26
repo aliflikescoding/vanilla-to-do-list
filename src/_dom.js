2e510fe37de71ad522298d81d97e91303d76ab27
aliflikescoding <alif.likes.coding@gmail.com>
2023-12-26 20:30:42 +0700

    Change the colors of the icon

--- src/_dom.js
+++ src/_dom.js
@@ -129,7 +129,7 @@ const Dom = (() => {
     }
 
     const trashIcon = addTrashIcon();
-    trashIcon.classList.add("clickable", "primary-color");
+    trashIcon.classList.add("clickable", "secondary-color");
     taskIcon.appendChild(trashIcon);
 
     //events
@@ -220,7 +220,7 @@ const Dom = (() => {
       loadProject(found, projectArray);
     });
     let projectIcon = document.createElement("i");
-    projectIcon.classList.add("clickable", "primary-color");
+    projectIcon.classList.add("clickable", "secondary-color");
     projectIcon.appendChild(addTrashIcon());
     projectIcon.addEventListener("click", () => {
       const projectArray = Dom.getProperObjectArray(
